0945096f80a27f75864704ad503d7bbcbc99bce5
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2024-08-02 20:11:03 +0100

    Added user account section

---
+++ cooking-forum/src/views/User.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { AppContext } from "../state/app.context";
+import { NavLink } from "react-router-dom";
+import UserPosts from "./UserPosts";
+
+export default function User() {
+  const { userData } = useContext(AppContext);
+
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
+
+  const { createdOn, handle, likePost } = userData;
+  const likedPosts = likePost ? Object.keys(likePost).length : 0;
+
+  return (
+    <div>
+      <h1>{handle}</h1>
+      <NavLink to="/user/my-posts">My Posts</NavLink><br />
+      <NavLink to="/user/liked-posts">Liked Posts: {likedPosts}</NavLink>
+      <p>Join Date: {new Date(createdOn).toLocaleDateString()}</p>
+    </div>
+  );
+}
