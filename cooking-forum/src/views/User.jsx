71f5c32a7d995e49ac7061331afbfb3bcb240da9
Radichev-Stoyan <stoyan.radichev.a62@learn.telerikacademy.com>
2024-08-04 11:49:46 +0100

    Liked posts counter in user profile now updates immediately without the need of refreshing the page

--- cooking-forum/src/views/User.jsx
+++ cooking-forum/src/views/User.jsx
@@ -1,16 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../state/app.context";
 import { NavLink } from "react-router-dom";
+import { onValue, ref } from "firebase/database";
+import { db } from "../config/firebase-config";
 
 export default function User() {
   const { userData } = useContext(AppContext);
+  const [likedPosts, setLikedPosts] = useState(0);
+
+  useEffect(() => {
+    if (userData && userData.handle) {
+      const userRef = ref(db, `users/${userData.handle}/likePost`);
+      return onValue(userRef, (snapshot) => {
+        if (snapshot.exists()) {
+          const posts = snapshot.val();
+          setLikedPosts(Object.keys(posts).length);
+        } else {
+          setLikedPosts(0);
+        }
+      });
+    }
+  }, [userData]);
 
   if (!userData) {
     return <div>Loading...</div>;
   }
 
-  const { createdOn, handle, likePost } = userData;
-  const likedPosts = likePost ? Object.keys(likePost).length : 0;
+  const { createdOn, handle } = userData;
+  // const likedPosts = likePost ? Object.keys(likePost).length : 0;
 
   return (
     <div>
