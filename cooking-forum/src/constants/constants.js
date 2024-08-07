b57959fc8f75d1856c1570384f346ff8ae620d32
Berkan Karaveli <berkankaraveli@abv.bg>
2024-08-07 20:31:42 +0300

    Added constants

--- cooking-forum/src/constants/constants.js
+++ cooking-forum/src/constants/constants.js
@@ -1,2 +1,6 @@
 export const MIN_NAME_LENGTH = 4;
 export const MAX_NAME_LENGTH = 32;
+export const MIN_TITLE_LENGTH = 16;
+export const MAX_TITLE_LENGTH = 64;
+export const MIN_CONTENT_LENGTH = 32;
+export const MAX_CONTENT_LENGTH = 8192;
