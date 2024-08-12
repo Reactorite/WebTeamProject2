69e36e42813dbedfdaf9293f6cc83176a7bfa1d2
Georgi Georgiev <gnikolaevv@gmail.com>
2024-08-12 09:58:12 +0300

    Header CSS changes

--- cooking-forum/src/components/Header.jsx
+++ cooking-forum/src/components/Header.jsx
@@ -5,6 +5,8 @@ import { logoutUser } from "../services/auth.service";
 import './Styles/Header.css';
 import Search from "../views/Search";
 
+
+
 export default function Header() {
   const { user, userData, setAppState } = useContext(AppContext);
   const navigate = useNavigate();
@@ -17,27 +19,33 @@ export default function Header() {
 
   return (
     <header>
+      {/* <div className="title-nav-wrapper"> */}
+      <div className="header-all">
       <div className="header-content">
-        <h1>UNITE <img src="../../public/React-Logo.png" alt="React Logo" className="reactLogo" /></h1>
+        <h1>UNITE</h1><img src="../../public/React-Logo.png" alt="React Logo" className="reactLogo" />
         <h4>REACT DEVELOPERS FORUM</h4>
       </div>
       <nav className="nav">
-        <NavLink to="/" className="nav-item">HOME</NavLink>
+        <NavLink to="/" className="nav-link">HOME</NavLink>
         {user && (
           <>
-            <NavLink to="/posts" className="nav-item">POSTS</NavLink>
-            <NavLink to="/posts-create" className="nav-item">CREATE</NavLink>
+            <NavLink to="/posts" className="nav-link">POSTS</NavLink>
+            <NavLink to="/posts-create" className="nav-link">CREATE</NavLink>
           </>
         )}
-        {!user && <NavLink to="/login" className="nav-item">LOGIN</NavLink>}
-        {!user && <NavLink to="/register" className="nav-item">REGISTER</NavLink>}
-        {user && <button onClick={logout} className="nav-button">LOGOUT</button>}
+        {!user && <NavLink to="/login" className="nav-link">LOGIN</NavLink>}
+        {!user && <NavLink to="/register" className="nav-link">REGISTER</NavLink>}
+        </nav>
         {user && userData && (
+        <div className="avatar-wrapper">
+          {user && <button onClick={logout} className="nav-button">LOGOUT</button>}
           <NavLink to="/user">
-            <img src={userData.profilePictureURL || `https://static.independent.co.uk/2022/06/28/10/anonymous%20terra%20luna%20crypto.jpg`} alt="Profile" className="profilePicture" />
+            <img src={userData.profilePictureURL || "../../images/default_avatar.png"} alt="Profile" className="profilePicture" />
           </NavLink>
+        </div>
         )}
-      </nav>
+
+      </div>
       <div className="search-wrapper">
         {user && <Search />}
       </div>
