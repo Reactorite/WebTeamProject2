b5cbc384dbfd61b459e31f82b3f405385d23ab05
Berkan Karaveli <berkankaraveli@abv.bg>
2024-08-12 15:13:19 +0300

    A lot of css changes - SinglePost, UserPosts,CreateComment,Comments,Posts + jsx

--- cooking-forum/src/components/Post.jsx
+++ cooking-forum/src/components/Post.jsx
@@ -1,22 +1,27 @@
-import PropType from 'prop-types';
+import PropTypes from 'prop-types';
 import { useContext } from 'react';
 import { AppContext } from '../state/app.context';
 import { dislikePost, likePost } from '../services/posts.service';
+import './Styles/Post.css'; 
+import { NavLink } from 'react-router-dom';
 
 /**
  * 
- * @param {{ post: {
- *  id: string,
- *  author: string,
- *  title: string,
- *  content: string,
- *  createdOn: string,
- *  likedBy?: string[]
- * } }} props 
- * @returns 
+ * @param {{
+ *  post: {
+ *    id: string,
+ *    author: string,
+ *    title: string,
+ *    content: string,
+ *    createdOn: string,
+ *    likedBy?: string[]
+ *  }
+ * }} props 
+ * @returns JSX.Element
  */
 export default function Post({ post }) {
   const { userData, isBlocked } = useContext(AppContext);
+
   const toggleLike = async () => {
     const isLiked = post.likedBy.includes(userData.handle);
     try {
@@ -31,22 +36,35 @@ export default function Post({ post }) {
   };
 
   return (
-    <div>
-      <h3>{post.title}</h3>
-      <p>{post.content}</p>
-      <p>Created on: {new Date(post.createdOn).toLocaleDateString()}</p>
-      {!isBlocked ? <button onClick={toggleLike}>{post.likedBy.includes(userData?.handle) ? 'Dislike' : 'Like'}</button> : "You cant act because you are banned"}
+    <div className="one-post-container">
+      <h3 className="one-post-title">{post.title}</h3>
+      <p className="one-post-content">{post.content}</p>
+      <div className="one-post-footer">
+        <p className="one-post-date">
+          Created on: {new Date(post.createdOn).toLocaleDateString()}
+        </p>
+        <p className="one-post-author">
+          Author: <NavLink to={`/single-user/${post.author}`}>{post.author}</NavLink>
+        </p>
+        {!isBlocked ? (
+          <button className="one-post-like-button" onClick={toggleLike}>
+            {post.likedBy.includes(userData?.handle) ? 'Dislike' : 'Like'}
+          </button>
+        ) : (
+          <p className="one-post-banned-message">You can't act because you are banned</p>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 Post.propTypes = {
-  post: PropType.shape({
-    id: PropType.string,
-    author: PropType.string,
-    title: PropType.string,
-    content: PropType.string,
-    createdOn: PropType.string,
-    likedBy: PropType.arrayOf(PropType.string),
-  })
-}
+  post: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    createdOn: PropTypes.string.isRequired,
+    likedBy: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
+};
