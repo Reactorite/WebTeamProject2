33f2dc6506ec357a4ea091e8ca5126bce9d7ebd2
Georgi Georgiev <gnikolaevv@gmail.com>
2024-08-13 20:27:34 +0300

    Footer Edit

--- react-dev-forum/src/components/Footer.jsx
+++ react-dev-forum/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import UserCount from '../views/UserCount';
 import PostCount from '../views/PostsCount';
+import './Styles/Footer.css';
 
 const Footer = () => {
   return (
     <footer>
-      <div>
+      <div className='footer'>
         <UserCount /> 
         <PostCount /> 
       </div>
